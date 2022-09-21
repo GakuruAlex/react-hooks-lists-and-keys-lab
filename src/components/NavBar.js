2dffb2dc2fbe5c500a7500c2f800013984606d5d
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-09-21 10:35:57 +0300

    Added NavBar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const userLinks= links.map((link)=>{
+
+    return <a href={link}>{link}</a>
+  }) ;
+
+  return <nav>{/* display an <a> tag for each link here */
+  userLinks}
+  </nav>;
 }
 
 export default NavBar;
